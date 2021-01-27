d4a325eee81290f82c60175f05ee867f0136b2c6
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2021-01-27 21:01:36 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,21 @@
+<?php
+$Host = "freemining.co";
+$sec-ch-ua = ""Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"";
+$sec-ch-ua-mobile = "?1";
+$upgrade-insecure-requests = "1";
+$user-agent = "Mozilla/5.0 (Linux; Android 6.0; Lenovo A6600d40) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+$accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+$sec-fetch-site = "same-origin";
+$sec-fetch-mode = "navigate";
+$sec-fetch-user = "?1";
+$sec-fetch-dest = "document";
+$referer = "https://freemining.co/556367?__cf_chl_jschl_tk__=4b479b0b54ece98003b7d7f209d8f8fd0b3b0f7d-1611753304-0-ATsnAC8fN00hXuHPQ9q75NYBOt9o7PHMgHkN2UYorNs1A7cg7NhMFzN4lqN60O1UTeZDX9fOnOqsmwn1AmwdbI3yTctJexVuaZ_CZXjYrtX_F19T_O6lOo5u_bQ08sikUGVbnEmyN9-4JNzEohS0d1q7Cdd---7XAJhzhGYfKcpQiF7xjcnQ7rnrj8unNC8GvvyBpfTCEkjE6YND3bdLajST60Y7ntwNZ00TzV6BGY7hRMKCjlfCsx1V-wGtBKhuYXndVXUgB41jchfU7xzwtSv-QqTi_tAmFObBeNq5i_WwL-0nmpq-EEgIcyD9F8FEEA";
+$accept-encoding = "gzip, deflate, br";
+$accept-language = "en-US,en;q=0.9";
+$cookie = "__cfduid=de9f78b8c0dba7015de2c1b90a12b25971610234155";
+$cookie = "_ga=GA1.2.145092982.1610234158";
+$cookie = "cf_chl_prog=a21";
+$cookie = "cf_clearance=4bd6eaceac29f4be6c9541c4b425557d20e4fab3-1611753317-0-150";
+$cookie = "PHPSESSID=shar7f8dfh69qqlo5kod69snr2";
+$cookie = "_gid=GA1.2.23192880.1611753323";
+$cookie = "_gat_gtag_UA_162747370_1=1";
